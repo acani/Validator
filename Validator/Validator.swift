1d0fa7e4dc505e888140efc3dbd4bc96eb7f8f40
Matthew Mario Di Pasquale <liveloveprosper@gmail.com>
2020-09-13 12:17:09 -0400

    Bump deployment target; update tests

--- Validator/Validator.swift
+++ Validator/Validator.swift
@@ -5,35 +5,18 @@ public let firstNameTooLongMessage = "First name is too long."
 public let lastNameRequiredMessage = "Last name is required."
 public let lastNameTooLongMessage = "Last name is too long."
 public let invalidUsernameMessage = "Username must be alphanumeric and between 1 & 64 characters."
-public let invalidEmailMessage = "Email must contain an at sign (@) and be between 3 & 254 characters."
+public let invalidEmailMessage =
+  "Email must contain an at sign (@) and be between 3 & 254 characters."
 public let invalidCodeMessage = "Code must be 6 digits."
 
 extension String {
-  public func strip() -> String {
-    return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-  }
-
+  public func strip() -> String { self.trimmingCharacters(in: .whitespacesAndNewlines) }
   public var isAlphanumeric: Bool {
-    return !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
+    !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
   }
-
-  public var isNumeric: Bool {
-    return !isEmpty && range(of: "[^0-9]", options: .regularExpression) == nil
-  }
-
-  public var isValidName: Bool {
-    return 1...3072 ~= count
-  }
-
-  public var isValidUsername: Bool {
-    return 1...64 ~= count && isAlphanumeric
-  }
-
-  public var isValidEmail: Bool {
-    return 3...254 ~= count && contains("@")
-  }
-
-  public var isValidCode: Bool {
-    return count == 6 && isNumeric
-  }
+  public var isNumeric: Bool { !isEmpty && range(of: "[^0-9]", options: .regularExpression) == nil }
+  public var isValidName: Bool { 1...3072 ~= count }
+  public var isValidUsername: Bool { 1...64 ~= count && isAlphanumeric }
+  public var isValidEmail: Bool { 3...254 ~= count && contains("@") }
+  public var isValidCode: Bool { count == 6 && isNumeric }
 }
--- ValidatorTests/ValidatorTests.swift
+++ ValidatorTests/ValidatorTests.swift
@@ -2,20 +2,30 @@ import XCTest
 import Validator
 
 class ValidatorTests: XCTestCase {
-  func testInvalidFirstNameMessage() {
-    XCTAssertEqual(invalidFirstNameMessage, "First name must be between 1 & 3,072 characters.")
+  func testFirstNameRequiredMessage() {
+    XCTAssertEqual(firstNameRequiredMessage, "First name is required.")
   }
 
-  func testInvalidLastNameMessage() {
-    XCTAssertEqual(invalidLastNameMessage, "Last name must be between 1 & 3,072 characters.")
+  func testFirstNameTooLongMessage() {
+    XCTAssertEqual(firstNameTooLongMessage, "First name is too long.")
+  }
+
+  func testLastNameRequiredMessage() {
+    XCTAssertEqual(lastNameRequiredMessage, "Last name is required.")
+  }
+
+  func testLastNameTooLongMessage() {
+    XCTAssertEqual(lastNameTooLongMessage, "Last name is too long.")
   }
 
   func testInvalidUsernameMessage() {
-    XCTAssertEqual(invalidUsernameMessage, "Username must be alphanumeric and between 1 & 64 characters.")
+    let expected = "Username must be alphanumeric and between 1 & 64 characters."
+    XCTAssertEqual(invalidUsernameMessage, expected)
   }
 
   func testInvalidEmailMessage() {
-    XCTAssertEqual(invalidEmailMessage, "Email must contain an at sign (@) and be between 3 & 254 characters.")
+    let expected = "Email must contain an at sign (@) and be between 3 & 254 characters."
+    XCTAssertEqual(invalidEmailMessage, expected)
   }
 
   func testInvalidCodeMessage() {
@@ -23,79 +33,82 @@ class ValidatorTests: XCTestCase {
   }
 
   func testStrip() {
-    XCTAssertEqual("".strip(), "")                      // empty
-    XCTAssertEqual("—Cool-".strip(), "—Cool-")          // dashes
-    XCTAssertEqual("  cool  ".strip(), "cool")          // spaces
-    XCTAssertEqual("\n cool \t".strip(), "cool")        // other
-    XCTAssertEqual("cool yo".strip(), "cool yo")        // normal
+    XCTAssertEqual("".strip(), "")                     // empty
+    XCTAssertEqual("—Cool-".strip(), "—Cool-")         // dashes
+    XCTAssertEqual("  cool  ".strip(), "cool")         // spaces
+    XCTAssertEqual("\n cool \t".strip(), "cool")       // other
+    XCTAssertEqual("cool yo".strip(), "cool yo")       // normal
   }
 
   func testIsAlphanumeric() {
-    XCTAssertFalse("".isAlphanumeric)                   // empty
-    XCTAssertFalse("Hello World".isAlphanumeric)        // space
-    XCTAssertFalse("7à123".isAlphanumeric)              // diacritic
-    XCTAssertFalse("mc²".isAlphanumeric)                // superscript
-    XCTAssertTrue("abc123".isAlphanumeric)              // normal
+    XCTAssertFalse("".isAlphanumeric)                  // empty
+    XCTAssertFalse("Hello World".isAlphanumeric)       // space
+    XCTAssertFalse("7à123".isAlphanumeric)             // diacritic
+    XCTAssertFalse("mc²".isAlphanumeric)               // superscript
+    XCTAssertTrue("abc123".isAlphanumeric)             // normal
     XCTAssertTrue("abcdefghijklmNOPQRSTUVWXYZ01234".isAlphanumeric)
     XCTAssertTrue("ABCDEFGHIJKLMnopqrstuvwxyz56789".isAlphanumeric)
   }
 
   func testIsNumeric() {
-    XCTAssertFalse("".isNumeric)                        // empty
-    XCTAssertFalse("12E3".isNumeric)                    // not numeric
-    XCTAssertFalse("23²3".isNumeric)                    // superscript
-    XCTAssertTrue("002".isNumeric)                      // zero first
-    XCTAssertTrue("1234".isNumeric)                     // normal
+    XCTAssertFalse("".isNumeric)                       // empty
+    XCTAssertFalse("12E3".isNumeric)                   // not numeric
+    XCTAssertFalse("23²3".isNumeric)                   // superscript
+    XCTAssertTrue("002".isNumeric)                     // zero first
+    XCTAssertTrue("1234".isNumeric)                    // normal
   }
 
   func testIsValidName() {
-    var longName = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ.0123456789`~!@#$%^&*()_—+={}[]\\|\"'?/<>,;:—"
+    var longName = """
+    abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.0123456789 \
+    `~!@#$%^&*()_—+={}[]\\|\"'?/<>,;:—
+    """
     longName = String(repeating: longName, count: 32)
     XCTAssertEqual(longName.count, 3072)
 
-    XCTAssertFalse("".isValidName)                      // empty / too short
-    XCTAssertFalse((longName+"1").isValidName)          // too long
-    XCTAssertTrue("a".isValidName)                      // shortest
+    XCTAssertFalse("".isValidName)                     // empty / too short
+    XCTAssertFalse((longName+"1").isValidName)         // too long
+    XCTAssertTrue("a".isValidName)                     // shortest
     XCTAssertTrue("1".isValidName)
-    XCTAssertTrue(longName.isValidName)                 // longest
-    XCTAssertTrue("John".isValidName)                   // normal
+    XCTAssertTrue(longName.isValidName)                // longest
+    XCTAssertTrue("John".isValidName)                  // normal
   }
 
   func testIsValidUsername() {
     let longUsername = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12"
     XCTAssertEqual(longUsername.count, 64)
 
-    XCTAssertFalse("".isValidUsername)                  // too short
-    XCTAssertFalse((longUsername+"3").isValidUsername)  // too long
-    XCTAssertFalse("John Smith".isValidUsername)        // not alphanumeric
+    XCTAssertFalse("".isValidUsername)                 // too short
+    XCTAssertFalse((longUsername+"3").isValidUsername) // too long
+    XCTAssertFalse("John Smith".isValidUsername)       // not alphanumeric
     XCTAssertFalse("John.Smith".isValidUsername)
-    XCTAssertTrue("a".isValidUsername)                  // shortest
+    XCTAssertTrue("a".isValidUsername)                 // shortest
     XCTAssertTrue("1".isValidUsername)
-    XCTAssertTrue(longUsername.isValidUsername)         // longest
-    XCTAssertTrue("abc123".isValidUsername)             // letters & numbers
-    XCTAssertTrue("johnsmith".isValidUsername)          // only letters
-    XCTAssertTrue("1234567890".isValidUsername)         // only numbers
+    XCTAssertTrue(longUsername.isValidUsername)        // longest
+    XCTAssertTrue("abc123".isValidUsername)            // letters & numbers
+    XCTAssertTrue("johnsmith".isValidUsername)         // only letters
+    XCTAssertTrue("1234567890".isValidUsername)        // only numbers
   }
 
   func testIsValidEmail() {
     var longEmail  = "max@"; for _ in 1...250 { longEmail += "x" }
     XCTAssertEqual(longEmail.count, 254)
 
-    XCTAssertFalse("".isValidEmail)                     // empty
-    XCTAssertFalse("a@".isValidEmail)                   // too short
-    XCTAssertFalse((longEmail+"1").isValidEmail)        // too long
-    XCTAssertFalse("no at-sign".isValidEmail)           // no at-sign
-    XCTAssertTrue("a2@".isValidEmail)                   // shortest
-    XCTAssertTrue(longEmail.isValidEmail)               // longest
-    XCTAssertTrue("me@example.com".isValidEmail)        // normal
+    XCTAssertFalse("".isValidEmail)                    // empty
+    XCTAssertFalse("a@".isValidEmail)                  // too short
+    XCTAssertFalse((longEmail+"1").isValidEmail)       // too long
+    XCTAssertFalse("no at-sign".isValidEmail)          // no at-sign
+    XCTAssertTrue("a2@".isValidEmail)                  // shortest
+    XCTAssertTrue(longEmail.isValidEmail)              // longest
+    XCTAssertTrue("me@example.com".isValidEmail)       // normal
   }
 
   func testIsValidCode() {
-    XCTAssertFalse("".isValidCode)                      // empty
-    XCTAssertFalse("002".isValidCode)                   // too short
-    XCTAssertFalse("12323".isValidCode)                 // too long
-    XCTAssertFalse("233²".isValidCode)                  // superscript
-    XCTAssertFalse("12E3".isValidCode)                  // not numeric
-    XCTAssertTrue("1234".isValidCode)                   // normal
+    XCTAssertFalse("".isValidCode)                     // empty
+    XCTAssertFalse("00221".isValidCode)                // too short
+    XCTAssertFalse("1232386".isValidCode)              // too long
+    XCTAssertFalse("23356²".isValidCode)               // superscript
+    XCTAssertFalse("1290E3".isValidCode)               // not numeric
+    XCTAssertTrue("123456".isValidCode)                // normal
   }
 }
